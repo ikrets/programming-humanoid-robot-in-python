7a96a312b5e19437e70adb6afaa133b5a06c27aa
Finnem <Finn-mier@web.de>
2015-11-08 00:03:09 +0100

    interpolation done

--- joint_control/angle_interpolation.py
+++ joint_control/angle_interpolation.py
@@ -22,7 +22,20 @@
 
 from pid import PIDAgent
 from keyframes import hello
+from keyframes import wipe_forehead
+from keyframes import leftBackToStand
+from math import fmod
+import numpy as np
+import matplotlib.pyplot as plt
+from spark_agent import INVERSED_JOINTS
 
+epsilon = 1e-6 #error margin for x to t conversion
+
+'''
+interpolatedPoints = [[],[]]
+keyframePoints = [[],[]]
+testJoint = "LElbowYaw"
+'''
 
 class AngleInterpolationAgent(PIDAgent):
     def __init__(self, simspark_ip='localhost',
@@ -32,16 +45,104 @@ class AngleInterpolationAgent(PIDAgent):
                  sync_mode=True):
         super(AngleInterpolationAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
         self.keyframes = ([], [], [])
+        self.myTime = self.perception.time
+        #self.done = 0 # only relevant for plotting
+        
 
     def think(self, perception):
         target_joints = self.angle_interpolation(self.keyframes)
         self.target_joints.update(target_joints)
-        return super(PIDAgent, self).think(perception)
+        return super(AngleInterpolationAgent, self).think(perception)
 
     def angle_interpolation(self, keyframes):
         target_joints = {}
+        
         # YOUR CODE HERE
-
+        time = self.perception.time - self.myTime
+        (names, times, keys) = keyframes
+        i = 0
+        for name in names:
+	  curTimes = times[i]
+	  
+	  if curTimes[-1]<time or time<curTimes[0]:
+	    '''
+	    #plot interpolated data for testing
+	    if (not self.done) and curTimes[-1]<time:
+	      self.done = 1
+	      plt.plot(interpolatedPoints[0],interpolatedPoints[1],"r")
+	      plt.plot(keyframePoints[0],keyframePoints[1],"bo")
+	      plt.title(testJoint)
+	      plt.show()
+	    '''
+	    continue
+	  
+	  #getting relevant Indices
+	  eIndex = len([x for x in curTimes if x<time])
+	  sIndex = eIndex - 1
+	  
+	  #get interpolation points
+	  skey = keys[i][sIndex]
+	  (p0x, p0y) = (curTimes[sIndex], skey[0])
+	  (p1x, p1y) = (p0x + skey[2][1], p0y + skey[2][2])
+	  ekey = keys[i][eIndex]
+	  (p3x, p3y) = (curTimes[eIndex], ekey[0])
+	  (p2x, p2y) = (p3x + ekey[1][1], p3y + ekey[1][2])
+	  
+	  #calculating bezier polynomial as described in http://pomax.github.io/bezierinfo/
+	  bezierMatrix = np.array([[1,0,0,0],[-3,3,0,0],[3,-6,3,0],[-1,3,-3,1]])
+	  x = np.array([p0x,p1x,p2x,p3x])
+	  y = np.array([p0y,p1y,p2y,p3y])
+	  
+	  #getting t value for curTime
+	  coefficientsX = np.dot(bezierMatrix, x)
+	  coefficientsX[0] -= time
+	  candidates = np.polynomial.polynomial.polyroots(coefficientsX)
+	  
+	  #finding correct candidate
+	  candidates = [x.real for x in candidates if -(epsilon)<=x.real<=1+(epsilon) and x.imag == 0] #error margin uncertain
+	  candidates = np.asarray([(x,np.abs(x-0.5)) for x in candidates],dtype = [("value", float),("distance", float)])
+	  candidates = np.sort(candidates, order="distance")
+	  
+	  t = candidates[0][0]
+	  if t < 0.: #clip values marginally smaller than 0 to 0
+	    t = 0.
+	  if t > 1.: #clip values marginally larger than 1 to 1
+	    t = 1.
+	    
+	  ''' testing t values close to boundaries
+	  if not t:
+	    print "t was empty"
+	  for x in t:
+	    if x >1:
+	      f = open("workfile","a+")
+	      f.write("[")
+	      for y in t:
+		f.write("%.32f,"%y)
+	      f.write("]")
+	      f.close()
+	  '''
+	  
+	  #getting y values
+	  coefficientsY = np.dot(bezierMatrix, y)
+	  result = np.dot(np.array([1, t, t**2, t**3]),coefficientsY)
+	  if name in INVERSED_JOINTS:
+	    target_joints[name] = -result
+	  else:
+	    target_joints[name] = result
+	  
+	  '''
+	  #collecting plot data
+	  if name == testJoint:
+	    interpolatedPoints[0].append(time)
+	    interpolatedPoints[1].append(result)
+	    keyframePoints[0].append(p0x)
+	    keyframePoints[1].append(p0y)
+	    keyframePoints[0].append(p3x)
+	    keyframePoints[1].append(p3y)
+	  '''
+	  
+	  i += 1
+        
         return target_joints
 
 if __name__ == '__main__':
