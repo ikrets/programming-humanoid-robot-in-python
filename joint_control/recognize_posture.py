b858010f43a5f4fc1d30ad964f473d783a3676e1
Finnem <Finn-mier@web.de>
2015-11-08 17:54:47 +0100

    recognize posture not quite working

--- joint_control/recognize_posture.py
+++ joint_control/recognize_posture.py
@@ -12,8 +12,13 @@
 
 from angle_interpolation import AngleInterpolationAgent
 from keyframes import hello
-
-
+from keyframes import leftBackToStand
+import numpy as np
+import pickle
+from sklearn import svm
+featureNames = ['AngleX', 'AngleY', 'LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch']
+classes = ['Knee', 'Back', 'Right', 'Crouch', 'Sit', 'Belly', 'StandInit', 'Stand', 'HeadBack', 'Frog', 'Left']
+ROBOT_POSE_CLF = 'robot_pose.pkl'
 class PostureRecognitionAgent(AngleInterpolationAgent):
     def __init__(self, simspark_ip='localhost',
                  simspark_port=3100,
@@ -22,7 +27,7 @@ class PostureRecognitionAgent(AngleInterpolationAgent):
                  sync_mode=True):
         super(PostureRecognitionAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
         self.posture = 'unknown'
-        self.posture_classifier = None  # LOAD YOUR CLASSIFIER
+        self.posture_classifier = pickle.load(open(ROBOT_POSE_CLF))  # LOAD YOUR CLASSIFIER
 
     def think(self, perception):
         self.posture = self.recognize_posture(perception)
@@ -31,10 +36,19 @@ class PostureRecognitionAgent(AngleInterpolationAgent):
     def recognize_posture(self, perception):
         posture = 'unknown'
         # YOUR CODE HERE
-
-        return posture
+        data = np.empty(len(featureNames))
+        data[0] = perception.imu[0]
+        data[1] = perception.imu[1]
+        i = 2
+        for feature in featureNames[i:]:
+	        data[i] = perception.joint[feature]
+	        i += 1
+        
+       
+        prediction = self.posture_classifier.predict(data)
+        return classes[prediction[0]]
 
 if __name__ == '__main__':
-    agent = AngleInterpolationAgent()
-    agent.keyframes = hello()  # CHANGE DIFFERENT KEYFRAMES
+    agent = PostureRecognitionAgent()
+    agent.keyframes = leftBackToStand()  # CHANGE DIFFERENT KEYFRAMES
     agent.run()
