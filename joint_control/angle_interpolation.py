65b86d6ee35e74c3002eb28c494ff22e0ddaf2ac
Ilia Krets <krets@campus.tu-berlin.de>
2018-11-21 18:49:34 +0100

    Implement angle interpolation client

--- joint_control/angle_interpolation.py
+++ joint_control/angle_interpolation.py
@@ -19,9 +19,11 @@
     # preceding the point, the second describes the curve following the point.
 '''
 
-
+import numpy as np
+import pickle
 from pid import PIDAgent
 from keyframes import hello
+from bezier_interpolators import BezierInterpolators
 
 
 class AngleInterpolationAgent(PIDAgent):
@@ -30,21 +32,53 @@ class AngleInterpolationAgent(PIDAgent):
                  teamname='DAInamite',
                  player_id=0,
                  sync_mode=True):
-        super(AngleInterpolationAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
-        self.keyframes = ([], [], [])
+        super(AngleInterpolationAgent, self).__init__(simspark_ip,
+                                                      simspark_port, teamname,
+                                                      player_id, sync_mode)
+        self.interpolators = None
+        self.motion_start = None
+        self.start_delay = 5
+
+        self.joints_log = []
+        self.target_joints_log = []
+        self.actions_log = []
 
     def think(self, perception):
-        target_joints = self.angle_interpolation(self.keyframes, perception)
+        print(perception.joint['LElbowYaw'], perception.joint['LElbowRoll'])
+        target_joints = self.angle_interpolation(perception)
         self.target_joints.update(target_joints)
-        return super(AngleInterpolationAgent, self).think(perception)
+        self.target_joints_log.append(self.target_joints.copy())
+        self.joints_log.append(perception.joint.copy())
 
-    def angle_interpolation(self, keyframes, perception):
-        target_joints = {}
-        # YOUR CODE HERE
+        result = super(AngleInterpolationAgent, self).think(perception)
+        self.actions_log.append(self.joint_controller.u.copy())
 
-        return target_joints
+        return result
+
+    def set_keyframes(self, keyframes):
+        # convert keyframes to bezier sections
+        names, times, keys = keyframes
+        self.interpolators = BezierInterpolators(names, times, keys)
+
+    def angle_interpolation(self, perception):
+        assert self.interpolators
+
+        if not self.motion_start:
+            self.motion_start = perception.time
+
+        result = self.interpolators.compute(perception.time - self.motion_start)
+        if not result:
+            exit()
+        return result
+
 
 if __name__ == '__main__':
+    np.seterr(all='raise')
     agent = AngleInterpolationAgent()
-    agent.keyframes = hello()  # CHANGE DIFFERENT KEYFRAMES
-    agent.run()
+    agent.set_keyframes(hello())
+    agent.start()
+
+    agent.thread.join()
+    with open('logs.pickle', 'wb') as fp:
+        pickle.dump(
+            (agent.joints_log, agent.target_joints_log, agent.actions_log), fp)
